c9cba81cc49747482225c2b800c226eb29166fe4
Warren Jonas <jonaswarren9@gmail.com>
2023-12-06 00:55:34 +0200

    config changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,32 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: `${process.env.CORS_ALLOW_CREDENTIALS}`,
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: `${process.env.CORS_ALLOW_ORIGIN}`,
+          }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: `${process.env.CORS_ALLOW_METHODS}`,
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: `${process.env.CORS_ALLOW_HEADERS}`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
